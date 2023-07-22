8136378b1f3c2169e80a20529af0640a91e876ef
Paul P <paukstel@umd.edu>
2023-07-22 09:22:45 -0400

    cleaner using pop

--- octoprint_bettergrblsupport/_bgs.py
+++ octoprint_bettergrblsupport/_bgs.py
@@ -825,7 +825,7 @@ def do_xy_probe(_plugin, axes, sessionId):
     frameOrigin = _plugin._settings.get(["frame_origin"])
     angle_probe = _plugin._settings.get(["angle_probe"])
     xy_steps = 3 #get as a setting
-    xy_probe_step = 3 #get as a setting
+    xy_probe_step = 3 #get as a setting, mm
     # do we do not support xy probe for center origins
     if "Center" in frameOrigin:
         _plugin._plugin_manager.send_plugin_message(_plugin._identifier, dict(type="simple_notify",
@@ -856,18 +856,17 @@ def do_xy_probe(_plugin, axes, sessionId):
                 "G21",
                 "G0 G91 X{} F{}".format(distance, xyf),
                 "G0 G91 Z{} F{}".format(15 * _plugin.invertZ * -1, zf),
+                "G38.2 X{} F200".format(distance * -1)
             ]
     #Multi-probe with Y increments
     if angle_probe:
+        gcode.pop()
         originInvertY = 1 if "Bottom" in frameOrigin else -1
         x_gcode = [
                     "G38.2 X{} F200".format(distance * -1),
                     "G0 G91 X{} Y{} F{}".format(distance, xy_probe_step*originInvertY, xyf)
         ]
         gcode.extend(x_gcode * xy_steps)
-
-    else:
-        gcode.append("G38.2 X{} F200".format(distance * -1))
     axis = "X"
 
     if xyProbe._step == 1 and axes != "X":
@@ -878,9 +877,11 @@ def do_xy_probe(_plugin, axes, sessionId):
                     "G21",
                     "G0 G91 Y{} F{}".format(distance, xyf),
                     "G0 G91 Z{} F{}".format(15 * _plugin.invertZ * -1, zf),
+                    "G38.2 Y{} F200".format(distance * -1)
                 ]
         #Multi-probe with X increments
         if angle_probe:
+            gcode.pop()
             originInvertX = 1 if "Left" in frameOrigin else -1
             y_gcode = [
                     "G38.2 Y{} F200".format(distance * -1),
@@ -888,9 +889,7 @@ def do_xy_probe(_plugin, axes, sessionId):
                     ]
         
             gcode.extend(y_gcode * xy_steps)
-
-        else:
-            gcode.append("G38.2 Y{} F200".format(distance * -1))
+            
         axis = "Y"
 
     elif len(xyProbe._results) > 1 or (len(xyProbe._results) > 0 and axis in ("X", "Y")):
